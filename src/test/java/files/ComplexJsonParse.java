aad717cf892a4ab874fc667b889cfbfc119ec4ca
Vahit Peker <vahit.peker@itadmins-MacBook-Air-9.local>
2020-08-29 14:12:32 -0600

    complexJsonParse

--- src/test/java/files/ComplexJsonParse.java
+++ src/test/java/files/ComplexJsonParse.java
@@ -3,7 +3,17 @@ package files;
 import io.restassured.path.json.JsonPath;
 
 public class ComplexJsonParse {
+    public static void main(String[] args) {
 
-    JsonPath js=new JsonPath(payload.courcePrice());
 
+        JsonPath js = new JsonPath(payload.courcePrice());
+
+        int count = js.getInt("courses.size()");
+        System.out.println(count);
+        //print purchase amount
+       int totalAmount= js.getInt("dashboard.purchaseAmount");
+        System.out.println(totalAmount);
+
+
+    }
 }
